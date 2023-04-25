a2eb2cf882fa1c7d6e712e70fa20e7b9e0fe9a40
olesia <Trapeznikova1803@gmail.com>
2023-04-25 21:56:05 +0300

    step 2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,7 +1,15 @@
 #!/usr/bin/env node
+
 import { Command } from 'commander/esm.mjs';
 const program = new Command();
+
+
+
 program
+  .arguments ('<filepath1> <filepath2>')
+  .description('Compares two configuration files and shows a difference.')
   .version('0.0.1', '-V, --version', 'output the version number')
-  .description('Compares two configuration files and shows a difference.');
+  .option('-f, --format <type>', 'output format')
+  
+  
 program.parse();
