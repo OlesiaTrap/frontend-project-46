2d45d815b1e12fd1358a28778d910acb074949a3
olesia <Trapeznikova1803@gmail.com>
2023-05-11 00:26:47 +0300

    step3

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,15 +1,20 @@
 #!/usr/bin/env node
 
-import { Command } from 'commander/esm.mjs';
+import { Command } from 'commander';
+import gendiff from '../src/index.js';
 const program = new Command();
 
 
 
 program
-  .arguments ('<filepath1> <filepath2>')
+  .name('gendiff')
+  .argument ('<filepath1>')
+  .argument ('<filepath2>')
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1', '-V, --version', 'output the version number')
   .option('-f, --format <type>', 'output format')
-  
+  .action ((filepath1, filepath2) => {
+  console.log(gendiff(filepath1, filepath2, program.opts().format));
+  });
   
 program.parse();
---
+++ src/index.js
@@ -0,0 +1,37 @@
+import fs from 'fs';
+import _ from 'lodash';
+import path from 'path';
+
+const getFullPath = (filepath) => path.resolve(process.cwd(), filepath);
+const getFileContent = (filepath) => fs.readFileSync(getFullPath(filepath), 'utf-8');
+
+const gendiff = (filepath1, filepath2) => {
+  const jsonData1 = JSON.parse(getFileContent(filepath1));
+  const jsonData2 = JSON.parse(getFileContent(filepath2));
+
+  const allKeys = _.union(Object.keys(jsonData1), Object.keys(jsonData2));
+  allKeys.sort();
+
+  let diffResult = "{\n";
+
+  for (let i = 0; i < allKeys.length; i++) {
+    const key = allKeys[i];
+    
+    if (!jsonData1.hasOwnProperty(key)) {
+      diffResult += `  + ${key}: ${jsonData2[key]}\n`;
+    } else if (!jsonData2.hasOwnProperty(key)) {
+      diffResult += `  - ${key}: ${jsonData1[key]}\n`;
+    } else if (jsonData1[key] === jsonData2[key]) {
+      diffResult += `   ${key}: ${jsonData1[key]}\n`;
+    } else {
+      diffResult += `  - ${key}: ${jsonData1[key]}\n  + ${key}: ${jsonData2[key]}\n`;
+    }
+  }
+
+  diffResult += "}";
+
+  return diffResult;
+}
+
+export default gendiff;
+
